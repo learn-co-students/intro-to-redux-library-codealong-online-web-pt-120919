7296f39026f6871942cfe5981b7945a5f0283f4b
fasnimo <fasnimo@gmail.com>
2020-09-26 21:08:15 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,13 +4,17 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+	console.log("call_to_action", action)
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log("Current state.items lenght %s", state.items.length)
+			console.log("Update state.items length to %s", state.items.length + 1)
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log("Initial state.items length %s", state.items.length)
 			return state;
 	}
 }
